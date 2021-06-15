e00251f9b62ab2b05dee287e031105dfb39cd232
SARA PERRY <SARAPERRYY@GMAIL.COM>
2021-06-14 19:45:39 -0500

    started js file for the profile intake info

---
+++ JS/profile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// can we use the user name window prompt as a place holder for a login page?
+let username = window.prompt('What is your name?');
+    if(username === '') {
+        alert('Please enter your name');
+    } else {
+        alert(`Thank you ${username}`);
+    }
+
+function userFirstName() {
+    let firstName = prompt('Enter your first name');
+    if ()
+}
+
+function userLastName() {
+    let lastName = prompt('Enter your last name')
+}
+
+function userAge() {
+    let age = prompt('Enter your age')
+}
+
+function userHeight() {
+    let height = prompt('Please enter your height in inches')
+}
+
+function userWeight() {
+    let weight = prompt('Please enter your weight in pounds')
+}
+
+function userGoalWeight() {
+    let goalWeight = prompt('Enter your goal weight in pounds')
+}
+
+
+
