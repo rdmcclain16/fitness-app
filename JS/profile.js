38be490e3bc2a08cda7c790e2b89b314d27af24b
Rosa Truneh <rosa.truneh@gmail.com>
2021-06-15 18:50:18 -0500

    working on getting prompt to open

---
+++ JS/profile.js
@@ -0,0 +1,39 @@
+'use strict';
+
+//dynamic table
+let form = document.getElementById('form');
+
+// can we use the user name window prompt as a place holder for a login page?
+let welcome = window.prompt('Thanks for choosing us!');
+
+let user = window.prompt('can we get your first name?')
+    if(username === '') {
+        alert('Please enter your name');
+    } else {
+        alert(`Thank you ${username}`);
+    };
+
+function userFirstName() {
+    let firstName = prompt('Enter your first name');
+    if ()
+}
+
+function userLastName() {
+    let lastName = prompt('Enter your last name')
+}
+
+function userAge() {
+    let age = prompt('Enter your age')
+}
+
+function userHeight() {
+    let height = prompt('Please enter your height in inches')
+}
+
+function userWeight() {
+    let weight = prompt('Please enter your weight in pounds')
+}
+
+function userGoalWeight() {
+    let goalWeight = prompt('Enter your goal weight in pounds')
+}
