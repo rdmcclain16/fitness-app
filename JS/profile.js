09b91a27c1e90ad52b7e25894e9da7bb39ad9a40
Rosa Truneh <rosa.truneh@gmail.com>
2021-06-15 19:37:13 -0500

    working to get info into form

--- JS/profile.js
+++ JS/profile.js
@@ -17,3 +17,19 @@ while (!user) {
 if (user.length > 0) {
 confirm('Hi ' + user + ' let\'s play!');
 }
+
+function showInput() 
+{   
+    let form = document.forms[0].elements;
+    let message_entered = "";
+
+    for(let i = 0, j = form.length;i < j;i++) 
+    {
+
+        if(form[i].type == "text") {message_entered += (form[i].value + " - ");}
+
+    }
+
+    document.getElementById('display').innerHTML = message_entered;
+
+}
