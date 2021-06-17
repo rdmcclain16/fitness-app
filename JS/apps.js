b6904fecb23be296c8e4c3cc668fe30219bc0c06
Kafele Sterling <kafelesterling@Kafeles-MacBook-Pro.local>
2021-06-16 18:34:12 -0700

    updated js with array of objects, deleted constructor functions

--- JS/apps.js
+++ JS/apps.js
@@ -1,41 +1,185 @@
-// OBJECT CONSTRUCTOR
-function Food(item, calories) {
-    this.item = item;
-    this.calories = calories;
-    this.updateItem = function () {
-        return ++this.item
-    };
-}
+// ARRAY OF OBJECTS
+let foods = [
+    {
+        name: 'broccoli',
+        foodType: 'veggies',
+        calories: 80
+    },
+    {
+        name: 'cabbage',
+        foodType: 'veggies',
+        calories: 21
+    },
+    {
+        name: 'carrots',
+        foodType: 'veggies',
+        calories: 52
+    },
+    {
+        name: 'celery',
+        foodType: 'vegetable',
+        calories: 6
+    },
+    {
+        name: 'brussels sprouts',
+        foodType: 'vegetable',
+        calories: 38
+    },
+    {
+        name: 'asparagus',
+        foodType: 'vegetable',
+        calories: 27
+    },
+    {
+        name: 'collard greens',
+        foodType: 'vegetable',
+        calories: 11
+    },
+    {
+        name: 'green beans',
+        foodType: 'vegetable',
+        calories: 34
+    },
+    {
+        name: 'string beans',
+        foodType: 'vegetable',
+        calories: 34
+    },
+    {
+        name: 'bell peppers',
+        foodType: 'vegetable',
+        calories: 30
+    },
+    {
+        name: 'spinach',
+        foodType: 'vegetable',
+        calories: 7
+    },
+    {
+        name: 'corn',
+        foodType: 'vegetable',
+        calories: 132
+    },
+    {
+        name: 'sweet potato',
+        foodType: 'vegetable',
+        calories: 112
+    },
+    {
+        name: 'potato',
+        foodType: 'vegetable',
+        calories: 168
+    },
+    {
+        name: 'cauliflower',
+        foodType: 'vegetable',
+        calories: 25
+    },
+    {
+        name: 'kale',
+        foodType: 'vegetable',
+        calories: 34
+    },
+    {
+        name: 'cucumber',
+        foodType: 'vegetable',
+        calories: 34
+    },
+    {
+        name: 'onion',
+        foodType: 'vegetable',
+        calories: 67
+    },
+    {
+        name: 'zuccini',
+        foodType: 'vegetable',
+        calories: 20
+    },
+    {
+        name: 'eggs',
+        foodType: 'protein',
+        calories: 78
+    },
+    {
+        name: 'almonds',
+        foodType: 'protein',
+        calories: 164
+    },
+    {
+        name: 'chicken breast',
+        foodType: 'protein',
+        calories: 284
+    },
+    {
+        name: 'chicken thigh',
+        foodType: 'protein',
+        calories: 226
+    },
+    {
+        name: 'chicken leg',
+        foodType: 'protein',
+        calories: 233
+    },
+    {
+        name: 'cottage cheee',
+        foodType: 'protein',
+        calories: 163
+    },
+    {
+        name: 'greek yogurt',
+        foodType: 'protein',
+        calories: 100
+    },
+    {
+        name: 'lean beef',
+        foodType: 'protein',
+        calories: 186
+    },
+    {
+        name: 'tuna',
+        foodType: 'protein',
+        calories: 128
+    },
+    {
+        name: 'quinoa',
+        foodType: 'protein',
+        calories: 222
+    },
+    {
+        name: 'salmon',
+        foodType: 'protein',
+        calories: 175
+    },
+    {
+        name: 'shrimp',
+        foodType: 'protein',
+        calories: 6
+    },
+    {
+        name: 'turkey breast',
+        foodType: 'protein',
+        calories: 146
+    },
+    {
+        name: 'turkey bacon',
+        foodType: 'protein',
+        calories: 31
+    },
+    {
+        name: 'turkey sausage',
+        foodType: 'protein',
+        calories: 56
+    },
+    {
+        name: 'chicken sausage',
+        foodType: 'protein',
+        calories: 49
+    },
 
-let food1 = new Food('Apple', 96);
-let food2 = new Food('Mango', 107);
-let food3 = new Food('Pineapple', 74);
-let food4 = new Food('Tomatoes', 22);
-let food5 = new Food('Nectarines', 60);
-let food6 = new Food('Cherries', 74);
-let food7 = new Food('Strawberries', 49);
-let food8 = new Food('Bananas', 105);
-let food9 = new Food('Grapes', 104);
-let food10 = new Food('Kiwi', 84);
-let food11 = new Food('Broccoli', 80);
-let food12 = new Food('Cabbage', 21);
-let food13 = new Food('Carrots', 52);
-let food14 = new Food('Celery', 6);
-let food15 = new Food('Brussels Sprouts', 38);
-let food16 = new Food('Asparagus', 27);
-let food17 = new Food('Collard Greens', 11);
-let food18 = new Food('Green Beans', 34);
-let food19 = new Food('String Beans', 34);
-let food20 = new Food('Bell Peppers', 30);
-let food21 = new Food('Eggs', 78);
-let food22 = new Food('Almonds', 164);
-let food23 = new Food('Chicken Breast', 284);
-let food24 = new Food('Cottage Cheese', 163);
-let food25 = new Food('Greek Yogurt', 100);
-let food26 = new Food('Lean Beef', 186);
-let food27 = new Food('Tuna', 128);
-let food28 = new Food('Quinoa', 222);
-let food29 = new Food('Salmon', 175);
-let food30 = new Food('Oats', 307);
 
-console.log(food1.updateItem());
+
+
+
+
+
+
