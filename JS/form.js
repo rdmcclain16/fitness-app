156640cffeec06af59a3b964f461d042e726fde4
Dondre <74502839+Drw2366@users.noreply.github.com>
2021-06-16 21:33:54 -0400

    started working on objects/arrays in javascript

--- JS/form.js
+++ JS/form.js
@@ -4,43 +4,156 @@
 let foods =[
     {
         name: 'apple',
-        foodType: 'fruits',
+        foodType: 'fruit',
         calories: 96,
 
     },
     {
-        name: 'bananas',
-        foodType: 'fruits',
+        name: 'banana',
+        foodType: 'fruit',
         calories: 105,
     },
     {
-        name:'mangos',
-        foodType: 'fruits',
+        name:'mango',
+        foodType: 'fruit',
         calories: 107,
 
     },
      {
-    name:'pineapples',
-    foodType: 'fruits',
+    name:'pineapple',
+    foodType: 'fruit',
     calories: 74,
 
     },
     {
-        name:'',
-        foodType:'',
-        calories:,
+        name:'kiwi',
+        foodType:'fruit',
+        calories: 84,
 
     },
     {
-        name:'',
-        foodType:'',
-        calories:,
+        name:'cherries',
+        foodType:'fruit',
+        calories: 74,
 
     },
     {
-        name:'',
-        foodType:'',
-        calories:,
+        name:'strawberries',
+        foodType:'fruit',
+        calories: 49,
 
+    },
+    {
+        name: 'grapes',
+        foodType: 'fruit',
+        calories: 104, 
+    },
+    {
+        name: 'tomato',
+        foodType: 'fruit',
+        calories: 22, 
+    },
+    {
+        name: 'blueberries',
+        foodType: 'fruit',
+        calories: 85, 
+    },
+    {
+        name: 'peach',
+        foodType: 'fruit',
+        calories: 50, 
+    },
+    {
+        name: 'raspberries',
+        foodType: 'fruit',
+        calories: 52, 
+    },
+    {
+        name: 'cantaloupe',
+        foodType: 'fruit',
+        calories: 186, 
+    },
+    {
+        name: 'watermelon',
+        foodType: 'fruit',
+        calories: 85, 
+    },
+    {
+        name: 'pear',
+        foodType: 'fruit',
+        calories: 102, 
+    },
+]
+
+let foods = [
+    {
+        name: 'one percent',
+        foodType: 'dairy/non-dairy',
+        calories: 103,
+    },
+    {
+        name: 'two percent',
+        foodType: 'dairy/non-dairy',
+        calories: 103,
+    },
+    {
+        name: 'almond milk',
+        foodType: 'dairy/non-dairy',
+        calories: 60,
+    },
+    {
+        name: 'coconut milk',
+        foodType: 'dairy/non-dairy',
+        calories: 34,
+    },
+    {
+        name: 'mozzarella',
+        foodType: 'dairy/non-dairy',
+        calories: 78,
+    },
+    {
+        name: 'cheddar',
+        foodType: 'dairy/non-dairy',
+        calories: 113,
+    },
+    {
+        name: 'parmesan',
+        foodType: 'dairy/non-dairy',
+        calories: 22,
+    },
+    {
+        name: 'butter',
+        foodType: 'dairy/non-dairy',
+        calories: 102,
+    },
+    {
+        name: 'greek yogurt',
+        foodType: 'dairy/non-dairy',
+        calories: 100,
+    },
+    {
+        name: 'low fat yogurt',
+        foodType: 'dairy/non-dairy',
+        calories: 100,
+    },
+    {
+        name: 'vanilla ice cream',
+        foodType: 'dairy/non-dairy',
+        calories: 137,
+    },
+    {
+        name: 'chocolate ice cream',
+        foodType: 'dairy/non-dairy',
+        calories: 143,
+    },
+    {
+        name: 'sour cream',
+        foodType: 'dairy/non-dairy',
+        calories: 23,
+    },
+    {
+        name: 'creamer',
+        foodType: 'dairy/non-dairy',
+        calories: 20,
     },
 ]
