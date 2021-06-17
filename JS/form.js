34422f098ceef9548adc4f4436f75756a087bdb9
Dondre <74502839+Drw2366@users.noreply.github.com>
2021-06-16 20:46:55 -0400

    added an array to js

---
+++ JS/form.js
@@ -0,0 +1,46 @@
+'use strict'
+
+
+let foods =[
+    {
+        name: 'apple',
+        foodType: 'fruits',
+        calories: 96,
+
+    },
+    {
+        name: 'bananas',
+        foodType: 'fruits',
+        calories: 105,
+    },
+    {
+        name:'mangos',
+        foodType: 'fruits',
+        calories: 107,
+
+    },
+     {
+    name:'pineapples',
+    foodType: 'fruits',
+    calories: 74,
+
+    },
+    {
+        name:'',
+        foodType:'',
+        calories:,
+
+    },
+    {
+        name:'',
+        foodType:'',
+        calories:,
+
+    },
+    {
+        name:'',
+        foodType:'',
+        calories:,
+
+    },
+]
