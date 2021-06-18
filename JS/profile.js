962a6cabc2fc54eaa635c5730f4b03d717865305
Rosa Truneh <rosa.truneh@gmail.com>
2021-06-17 19:17:19 -0500

    added function to profile js and html

--- JS/profile.js
+++ JS/profile.js
@@ -1,22 +1,6 @@
 // 'use strict'
 // //asking user if they want to play
 
-let answer1 = confirm('Would you like to sign up?')
-if (!answer1) {
-  alert('Sorry to see you go');
-} else {
-  alert('Great, let\'s get started!');
-}
-
-//once user says ok, ask for name then enter the page
-let user = prompt('what is your name?');
-while (!user) {
-  alert ('Seriously, I need your name.');
-  user = prompt('What is your name?');
-} 
-if (user.length > 0) {
-confirm('Hi ' + user + ' let\'s play!');
-}
 
 function showInput() 
 {   
@@ -33,3 +17,47 @@ function showInput()
     document.getElementById('display').innerHTML = message_entered;
 
 }
+
+
+function newUser (firstName, lastName, age, height, weight, goalWeight) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+    this.height = height;
+    this.weight = weight;
+    this.goalWeight = goalWeight;
+
+}
+
+let username = window.prompt('What is your name?');
+    if(username === '') {
+        alert('Please enter your name');
+    } else {
+        alert(`Thank you ${username}`);
+    }
+
+function userFirstName() {
+    let firstName = prompt('Enter your first name');
+    if ()
+}
+
+function userLastName() {
+    let lastName = prompt('Enter your last name')
+}
+
+function userAge() {
+    let age = prompt('Enter your age')
+}
+
+function userHeight() {
+    let height = prompt('Please enter your height in inches')
+}
+
+function userWeight() {
+    let weight = prompt('Please enter your weight in pounds')
+}
+
+function userGoalWeight() {
+    let goalWeight = prompt('Enter your goal weight in pounds')
+}
+
