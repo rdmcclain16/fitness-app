ad3c4ecc3f70d6c0055e91763c5cb21c5fe7f57e
Rosa Truneh <rosa.truneh@gmail.com>
2021-06-17 19:23:26 -0500

    updating

--- JS/profile.js
+++ JS/profile.js
@@ -1,22 +1,26 @@
 // 'use strict'
 // //asking user if they want to play
 
+function results(){
+    let firstName = document.getElementById("firstName").value;
+ 
+    let result ='First Name: ' + firstName;
+    document.getElementById("display").textContent = result;
+// function showInput() 
+// {   
+//     let form = document.forms[0].elements;
+//     let message_entered = "";
 
-function showInput() 
-{   
-    let form = document.forms[0].elements;
-    let message_entered = "";
+//     for(let i = 0, j = form.length;i < j;i++) 
+//     {
 
-    for(let i = 0, j = form.length;i < j;i++) 
-    {
+//         if(form[i].type == "text") {message_entered += (form[i].value + " - ");}
 
-        if(form[i].type == "text") {message_entered += (form[i].value + " - ");}
+//     }
 
-    }
+//     document.getElementById('display').innerHTML = message_entered;
 
-    document.getElementById('display').innerHTML = message_entered;
-
-}
+// }
 
 
 function newUser (firstName, lastName, age, height, weight, goalWeight) {
