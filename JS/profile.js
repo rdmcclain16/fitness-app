2a86fe964112cf8235eca09cf77270672d6f0dda
Rosa Truneh <rosa.truneh@gmail.com>
2021-06-19 12:08:32 -0500

    got information to display underneath form

--- JS/profile.js
+++ JS/profile.js
@@ -12,11 +12,32 @@
 // }
 function results() {
     
-    document.getElementById("display").innerHTML= "First name: " +
+    document.getElementById("first").innerHTML= "First name: " +
     document.getElementById("firstName").value;
  
+    document.getElementById("last").innerHTML= "Last name: " +
+    document.getElementById("lastName").value;
 
+    document.getElementById("age").innerHTML= "Age: " +
+    document.getElementById("userAge").value;
 
+    document.getElementById("height").innerHTML= "Height: " +
+    document.getElementById("userHeight").value;
+
+    document.getElementById("weight").innerHTML= "Weight: " +
+    document.getElementById("userWeight").value;
+    
+    document.getElementById("goal").innerHTML= "Goal Weight: " +
+    document.getElementById("userGoalWeight").value;
+
+    document.getElementById("email").innerHTML= "Email: " +
+    document.getElementById("userEmail").value;
+
+    document.getElementById("number").innerHTML= "Phone Number: " +
+    document.getElementById("userPhoneNumber").value;
+
+    document.getElementById("address").innerHTML= "Address: " +
+    document.getElementById("userAddress").value;
 }
 
     // let form = document.forms[0].elements;
