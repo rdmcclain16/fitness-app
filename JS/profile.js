35df977bed87f77c0be48d9a6471334a081feaf8
Rosa Truneh <rosa.truneh@gmail.com>
2021-06-19 11:47:23 -0500

    got first name to show

--- JS/profile.js
+++ JS/profile.js
@@ -1,21 +1,31 @@
 // 'use strict'
-// //asking user if they want to play
 
-function results(){
-    let firstName = document.getElementById("firstName").value;
+// function results(){
+//     let firstName = document.getElementById("firstName").value;
+//     let result ='First Name: ' + firstName;
+//     document.getElementById("display").textContent = result;
+
+//     let lastName = document.getElementById("lastName").value;
+//     let result ='Last Name: ' + lastName;
+//     document.getElementById("display").textContent = result;
+
+// }
+function results() {
+    
+    document.getElementById("display").innerHTML= "First name: " +
+    document.getElementById("firstName").value;
  
-    let result ='First Name: ' + firstName;
-    document.getElementById("display").textContent = result;
+
+
 }
-// function showInput() 
-// {   
-//     let form = document.forms[0].elements;
-//     let message_entered = "";
 
-//     for(let i = 0, j = form.length;i < j;i++) 
-//     {
+    // let form = document.forms[0].elements;
+    // let message_entered = "";
 
-//         if(form[i].type == "text") {message_entered += (form[i].value + " - ");}
+    // for(let i = 0, j = form.length;i < j;i++) 
+    // {
+
+    //     if(form[i].type == "text") {message_entered += (form[i].value + " - ");}
 
 //     }
 
