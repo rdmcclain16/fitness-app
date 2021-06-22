b3f28291b7d5ee24894acdf2bf47243d05ef8e3c
Rosa Truneh <rosa.truneh@gmail.com>
2021-06-21 19:59:52 -0500

    got console to log all input values

--- JS/form.js
+++ JS/form.js
@@ -1,6 +1,26 @@
 
 
+function getFruitValue(){
+    let getFruit = document.getElementById("qtyFruit").value;
+    console.log(getFruit);
+}
+
+function getVegValue(){
+    let getVeg = document.getElementById("qtyVeg").value;
+    console.log(getVeg);
+}
+
+function getProteinValue(){
+    let getProtein = document.getElementById("qtyProtein").value;
+    console.log(getProtein);
+}
+
 function getCarbValue(){
     let getCarb = document.getElementById("qtyCarb").value;
     console.log(getCarb);
+}
+
+function getDairyValue(){
+    let getDairy = document.getElementById("qtyDairy").value;
+    console.log(getDairy);
 }
