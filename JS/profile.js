85296900cce4d0372925c88be5766550fbcdd2aa
SARA PERRY <SARAPERRYY@GMAIL.COM>
2021-06-21 19:32:47 -0500

    updated profile

--- JS/profile.js
+++ JS/profile.js
@@ -1,50 +1,3 @@
-<<<<<<< HEAD
-'use strict';
-
-
-function newUser (firstName, lastName, age, height, weight, goalWeight) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.height = height;
-    this.weight = weight;
-    this.goalWeight = goalWeight;
-
-}
-
-let username = window.prompt('What is your name?');
-    if(username === '') {
-        alert('Please enter your name');
-    } else {
-        alert(`Thank you ${username}`);
-    }
-
-function userFirstName() {
-    let firstName = prompt('Enter your first name');
-    if ()
-}
-
-function userLastName() {
-    let lastName = prompt('Enter your last name')
-}
-
-function userAge() {
-    let age = prompt('Enter your age')
-}
-
-function userHeight() {
-    let height = prompt('Please enter your height in inches')
-}
-
-function userWeight() {
-    let weight = prompt('Please enter your weight in pounds')
-}
-
-function userGoalWeight() {
-    let goalWeight = prompt('Enter your goal weight in pounds')
-}
-
-=======
 // 'use strict'
 // //asking user if they want to play
 
@@ -69,6 +22,5 @@ function results(){
 //     document.getElementById('display').innerHTML = message_entered;
 
 // }
->>>>>>> 7ed9cab908451551f2ada773891cc56e85465b75
 
 
